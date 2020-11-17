e267518a5c44f8e3eecdd9c9b14f3e3bd1c6c515
Yadiel Shamdee <yadielshamdee@gmail.com>
2020-11-17 18:20:19 -0500

    testing auto update

--- BetterDiscord/Plugins/Senpai.plugin.js
+++ BetterDiscord/Plugins/Senpai.plugin.js
@@ -1,7 +1,13 @@
 /**
  * @name Senpai Plugin
  * @version 0.0.2
+ * @source https://raw.githubusercontent.com/YadMations/YadMations/main/BetterDiscord/Plugins/Senpai.plugin.js
  */
+
+const fs = require('fs')
+const path = require('path')
+
+const META_txt = ` * @version`
 
 const config = {
     "plugin": {
@@ -29,28 +35,44 @@ module.exports =
         load() {
         } // Called when the plugin is loaded in to memory
     
-        start() {
-            // if(this.getVersion() !== "0.0.2") {
-            //     BdApi.showConfirmationModal("Outdated Version Nya! ùnú", `Download lastest version of ${config.plugin.name} ${config.plugin.type} <3`, {
-            //         confirmText: 'Download Now owo',
-            //         cancelText: 'Cancel unu',
-            //         onCancel: _ => {BdApi.showToast("Hmph... Maybe next time! <3", {type: "error", icon: false})},
-            //         onConfirm: _ => {
-            //             this.getRawPlugin()
-            //         }
-            //     })
-            // }
+        async start() {
             BdApi.showToast("Senpai Plugin has been started! OwO", {type: "success"})
+            fetch(config.plugin.source)
+                .then(res => res.text())
+                .then(text => {
+                    if(typeof text !== "undefined" && typeof text == "string") {
+                        if(text.includes(META_txt)) {
+                            if(config.plugin.version !== text.substring(39, 44)) {
+                                BdApi.showConfirmationModal("Outdated Version Nya! ùnú", `Download lastest version of ${config.plugin.name} ${config.plugin.type} <3`, {
+                                    confirmText: 'Download Now owo',
+                                    cancelText: 'Cancel unu',
+                                    onCancel: _ => {BdApi.showToast("Hmph... Maybe next time! <3", {type: "error", icon: false})},
+                                    onConfirm: _ => {
+                                        fetch(config.plugin.source)
+                                           .then(res => res.text())
+                                           .then(text => {
+                                               fs.writeFile(path.join(__dirname + "Senpai.plugin.js"), text, {encoding: 'utf-8'}, (err) => {
+                                                   if(err) return BdApi.showToast("Couldn't update the plugin! Dx", {type: 'error'})
+                                                   BdApi.showToast("Plugin Updated Succesfully! OwO", {type: 'success'})
+                                               })
+                                           })
+                                    }
+                                })
+                            }
+                        }
+                    } else return BdApi.showToast("Error fetching latest version of the plugin > ~<", {type: "error"})
+                })
         } // Called when the plugin is activated (including after reloads)
+
         stop() {
             BdApi.showToast("Senpai Plugin has been stopped! See ya later <3", {type: "success"})
         } // Called when the plugin is deactivated
 
-        getRawPlugin() {
+        getRawPlugin(callback) {
             fetch(config.plugin.source, {method: "GET"}).then((res) => {
                 res.text()
             }).then((text) => {
-                return text;
+                return callback(text);
             })
         }
     }
